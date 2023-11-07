3083568f15433bf436a0d85bdc6a76d1e17ea6ac
Dinkar850 <guptadinkar850@gmail.com>
2023-11-07 23:33:03 +0530

    Splash screen
    Implements comments to splash screen

--- app/src/main/java/com/example/ces_app/Splashscreen.kt
+++ app/src/main/java/com/example/ces_app/Splashscreen.kt
@@ -8,14 +8,20 @@ import android.os.Looper
 import com.example.ces_app.databinding.ActivitySplashscreenBinding
 
 class Splashscreen : AppCompatActivity() {
+    //creating a variable for view binding
     private lateinit var binding: ActivitySplashscreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding view to our activity
         binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //creating a delay for our intent
         Handler(Looper.getMainLooper()).postDelayed({
+            //define a variable for intent passing
              val intent = Intent(this@Splashscreen, MainActivity::class.java)
+            //passing intent
             startActivity(intent)
+            //back button doesn't bring the previous activity back again
             finish()
         }, 3000)
     }
