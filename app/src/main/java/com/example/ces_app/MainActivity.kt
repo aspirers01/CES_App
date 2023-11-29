7a685db0c5eb29147bda3e4e76fc17c0182ffa28
Dinkar850 <guptadinkar850@gmail.com>
2023-11-30 01:05:39 +0530

    corrections added

--- app/src/main/java/com/example/ces_app/MainActivity.kt
+++ app/src/main/java/com/example/ces_app/MainActivity.kt
@@ -12,6 +12,8 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    //variable for view binding
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,55 +21,70 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //variables for different views
+        val userNameTil = binding.userName
+        val userEmailTil = binding.userEmail
+        val profilePicture = binding.profileImage
+        val exit = binding.exit
+        val signout = binding.signOut
+
         //getting current user
         val user = Firebase.auth.currentUser
-        user?.let {
-            // Name, email address, and profile photo Url
-            val name = it.displayName
-            val email = it.email
-            val photoUrl = it.photoUrl
-            val uid = it.uid
-        }
-        var name = user?.displayName
-        var domain: String? = null
-        val email = user?.email
 
-        //setting username as email address's name before @
+        //fetching current user's data
+        var name = user?.displayName
+        val email = user?.email
+        val photoUrl = user?.photoUrl
+
+        //for storing username if not signed in by google
+        var domain: String? = null
+
+        //setting username as email address before @
         if (email != null) {
             val index = email.indexOf('@')
             domain = if (index == -1) null else email.substring(0, index)
         }
-
         if (name == null) {
             name = domain
         }
-        val photoUrl = user?.photoUrl
-        val userNameTil = binding.userName
-        val userEmailTil = binding.userEmail
-        val profilePicture = binding.profileImage
+
+        //filling username in name textView
         userNameTil.text = name
+        //filling email of user in email textView
         userEmailTil.text = email
+
+        //if no profile photo in case of email password sign in, set uri as uri of drawable ic_profile
         if (photoUrl == null) {
             val uri: Uri =
                 Uri.parse("android.resource://" + packageName + "/" + "/drawable/ic_profile")
+
+            //set that image in profile picture imageView
             Glide.with(this).load(uri).into(profilePicture)
         } else {
+
+            //set image received from google account in imageView
             Glide.with(this).load(photoUrl).into(profilePicture)
         }
 
 
-        val exit = binding.exit
 
-        val signout = binding.signOut
-
+        //on clicking signOut button
         signout.setOnClickListener {
+            //firebase sign out
             Firebase.auth.signOut()
+
+            //display sign out toast
             Toast.makeText(this, "Signed Out!", Toast.LENGTH_LONG).show()
+
+            //intent for returning to Login activity
             intent = Intent(this, Login_UI::class.java)
             startActivity(intent)
+
+            //finish previous activity
             finish()
         }
 
+        //on clicking exit button, exit app
         exit.setOnClickListener {
             finish()
         }
